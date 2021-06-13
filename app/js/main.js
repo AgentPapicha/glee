265753db94725903bd820eea6f9a9de1e9d513f8
AgentPapicha <xlolshara@mail.ru>
2021-06-13 18:15:13 +0300

    added top-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,14 @@
-$(function(){
+$(function () {
 
     $('.nav ul').on('click', function () {
         $('.menu__list-link').toggleClass('menu__list-link--active');
     });
 
-}
-);
+    $('.top-slider__wrapper').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+});
