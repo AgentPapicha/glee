4542449f43a52d4c763ba18a7532b0e5dc93af41
AgentPapicha <xlolshara@mail.ru>
2021-08-23 21:55:24 +0300

    added price filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,43 @@
 $(function () {
 
+   $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+
+    onStart: function (data) {
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to); 
+    },
+
+    onChange: function (data) {
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+    },
+   });
+
+
+
+    $('.category-input__chair').on('click', function () {
+        $('.category-label__chair').toggleClass('filter-category__label--active');
+    });
+
+    $('.category-input__table').on('click', function () {
+        $('.category-label__table').toggleClass('filter-category__label--active');
+    });
+
+    $('.category-input__lamp').on('click', function () {
+        $('.category-label__lamp').toggleClass('filter-category__label--active');
+    });
+
+    $('.category-input__watch').on('click', function () {
+        $('.category-label__watch').toggleClass('filter-category__label--active');
+    });
+
+    $('.category-input__clock').on('click', function () {
+        $('.category-label__clock').toggleClass('filter-category__label--active');
+    });
+
+
     $('.nav ul').on('click', function () {
         $('.menu__list-link').toggleClass('menu__list-link--active');
     });
